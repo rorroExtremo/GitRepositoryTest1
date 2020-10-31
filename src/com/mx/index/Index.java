72b0742c90d476e71ff50f1591b34ddc2dd670df
Developer <Developer@LAPTOP-HP>
2020-10-31 08:12:57 -0600

    RorroExtremo2 update

--- src/com/mx/index/Index.java
+++ src/com/mx/index/Index.java
@@ -3,7 +3,7 @@ package com.mx.index;
 public class Index {
 
 	public static void main(String[] args) {
-		System.out.println("Hola proyecto para GIT1111");
+		System.out.println("Hola proyecto para GIT1111 - Actualizacion desde Rorro2");
 
 	}
 
