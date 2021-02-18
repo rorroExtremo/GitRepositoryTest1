ecf796ab73c6dffff1c721aabc717191e3fa8747
Rorro Perez <rorro2@hotmail.com>
2021-02-18 16:24:41 -0600

    Primer Drool commit

---
+++ src/main/java/com/mx/example/model/Product.java
@@ -0,0 +1,35 @@
+package com.mx.example.model;
+
+public class Product {
+
+	private String type;
+	private int discount;
+	private String detalle;
+
+
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+}
