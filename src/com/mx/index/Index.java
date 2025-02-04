36b9648470154d501e3b31294342f0d3d30b712d
rodrigo.davalos <rodrigo.davalos@MXLAGIL9965>
2025-02-04 14:20:09 -0600

    Commit desde eclipse

--- src/com/mx/index/Index.java
+++ src/com/mx/index/Index.java
@@ -4,7 +4,7 @@ public class Index {
 
 	public static void main(String[] args) {
 		System.out.println("Hola proyecto para GIT2222 - Actualizacion desde Rorro2");
-		
+		System.out.println("Hola proyecto para GIT2222 - Actualizacion desde Rorro2");
 	}
 
 }
