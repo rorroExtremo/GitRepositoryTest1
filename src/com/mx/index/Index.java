06bfc356469e53b38d5dcdcfebaef62582c8701a
Rodrigo Arturo Davalos Peña <rodrigo.davalos@motivus.com>
2025-02-04 10:52:59 -0600

    Mi pruebas trabajabdo en CiBanco1

--- src/com/mx/index/Index.java
+++ src/com/mx/index/Index.java
@@ -3,8 +3,8 @@ package com.mx.index;
 public class Index {
 
 	public static void main(String[] args) {
-		System.out.println("Hola proyecto para GIT1111 - Actualizacion desde Rorro2");
-
+		System.out.println("Hola proyecto para GIT2222 - Actualizacion desde Rorro2");
+		
 	}
 
 }
